f887ab6f52ac939681ccdab87f522737faa03ffe
Tikout <131374666+Tikout@users.noreply.github.com>
2023-05-17 22:46:29 +0200

    Delete 1.ify directory

--- 1.ify/Wiek.py
+++
@@ -1,7 +0,0 @@
-wiek = input("Podaj swoj wiek")
-y = int(wiek)
-if y < 18:
-    print ("Masz wiecej niz 18 lat")
-    pass
-else:
-    print ("Jestes za mlody !")
--- 1.ify/od 0 do 100.py
+++
@@ -1,1 +0,0 @@
-print(*[x for x in range(1,100) if not x % 3])

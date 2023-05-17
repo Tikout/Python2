b09142e73f95424bc4e2a97d9f08df811184e091
Tikout <131374666+Tikout@users.noreply.github.com>
2023-05-17 23:40:05 +0200

    Add files via upload

--- 8.Klasy_i_obiekty/Klasy.py
+++ 8.Klasy_i_obiekty/Klasy.py
@@ -1,9 +1,28 @@
 class Czytelnik:
-    Ja = "Mistrz"
+    def __init__(self,imie,nazwisko,wiek):
+        self.imie = imie
+        self._nazwisko = nazwisko
+        self._wiek = wiek
 
-class Liczby:
-    jeden = 1
-    dwa = 2
+def pobierz_wiek(self) -> int:
+    return self._wiek
+def pobierz_imie(self) -> str:
+    return self.imie
 
-class Parzyste(Liczby):
-    pass
+czytelnik1 = Czytelnik(
+    imie= "Mateusz",
+    nazwisko="Mąciwor",
+    wiek= 21
+)
+
+czytelnik2 = Czytelnik(
+    imie= "Natalia",
+    nazwisko="Muszka",
+    wiek= 21
+)
+
+print(pobierz_imie(czytelnik1))
+print(pobierz_wiek(czytelnik1))
+
+print(pobierz_imie(czytelnik2))
+print(pobierz_wiek(czytelnik2))

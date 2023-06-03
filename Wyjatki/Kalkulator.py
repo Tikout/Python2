64180a47a5217a072aa51df73db334bf3e80e00b
Tikout <131374666+Tikout@users.noreply.github.com>
2023-06-03 23:45:23 +0200

    Add files via upload

--- Wyjatki/Kalkulator.py
+++ Wyjatki/Kalkulator.py
@@ -9,7 +9,7 @@ def Mnozenie(x, y):
 
 def Dzielenie(x, y):
     return x / y
-
+    
 
 print("Wybierz operacje")
 print("1.Dodawanie")
@@ -17,33 +17,54 @@ print("2.Odejmowanie")
 print("3.Mnozenie")
 print("4.Dzielenie")
 
-while True:
-    wybor = input("Wybierz dzialanie(1/2/3/4): ")
+def Kalkulator():
+    while True:
+        wybor = input("Wybierz dzialanie(1/2/3/4): ")
 
-    if wybor in ('1', '2', '3', '4'):
-        try:
-            liczba1 = float(input("Wpisz pierwsza liczbe: "))
-            liczba2 = float(input("Wpisz druga liczbe: "))
-        except ValueError as ERROR:
-            print("Cos jest nie tak, stosuj sie do polecen.")
-            print(ERROR)
-            print("Sprobuj jeszcze raz")
-            continue
+        if wybor in ('1', '2', '3', '4'):
+            try:
+                liczba1 = float(input("Wpisz pierwsza liczbe: "))
+                liczba2 = float(input("Wpisz druga liczbe: "))
+            except ValueError as ERROR:
+                print("Cos jest nie tak, stosuj sie do polecen.")
+                print(ERROR)
+                print("Sprobuj jeszcze raz")
+                continue
 
-        if wybor == '1':
-            print(liczba1, "+", liczba2, "=", Dodawanie(liczba1, liczba2))
+            if wybor == '1':
+                print(liczba1, "+", liczba2, "=", Dodawanie(liczba1, liczba2))
 
-        elif wybor == '2':
-            print(liczba1, "-", liczba2, "=", Odejmowanie(liczba1, liczba2))
+            elif wybor == '2':
+                print(liczba1, "-", liczba2, "=", Odejmowanie(liczba1, liczba2))
 
-        elif wybor == '3':
-            print(liczba1, "*", liczba2, "=", Mnozenie(liczba1, liczba2))
+            elif wybor == '3':
+                print(liczba1, "*", liczba2, "=", Mnozenie(liczba1, liczba2))
 
-        elif wybor == '4':
-            print(liczba1, "/", liczba2, "=", Dzielenie(liczba1, liczba2))
-        
-        Nowe_dzialanie = input("Nowe dzialanie ? (Tak/Nie): ")
-        if Nowe_dzialanie == "Nie" or "nie":
-          break
-    else:
-        print("Cos jest nie tak")
+            elif wybor == '4':
+                if liczba2 == 0:
+                    print("Błąd ! Nie można dzielić prez 0 !")
+                else:
+                    print(liczba1, "/", liczba2, "=", Dzielenie(liczba1, liczba2))
+
+# Po usunięciu lub zakomentowaniu Nowe_Dzialanie program przestaje działać dobrze
+
+            Nowe_dzialanie = ("Nowe dzialanie ? (Tak/Nie): ")
+            if Nowe_dzialanie == "Nie" or "nie":
+                break
+            elif Nowe_dzialanie == "Tak" or "tak":
+                Kalkulator()
+
+def menu():
+   while True:
+       print('(1) Użyj kalkulatora')
+       print('(Q) Wyjdz')
+       choice = input('Co chcesz zrobić ?: ').lower()
+       if choice == '1':
+           Kalkulator()
+       elif choice == 'q':
+           return False
+       else:
+           print(f'Cos jest nie tak: {choice}')
+
+if __name__ == '__main__':
+    menu()
